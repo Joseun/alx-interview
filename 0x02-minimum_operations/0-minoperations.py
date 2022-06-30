9c347a9aa375096acbab8839d0f5ed2d3ee131c9
Joseun <ologanj@gmail.com>
2022-06-30 15:07:45 +0000

    Minimum Operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -11,12 +11,32 @@ def minOperations(n: int) -> int:
     Argument:
     n: integer
     """
-    count = 0
-    for i in range(2, n + 1):
-        # check if problem can be broken into smaller problem
-        while not n % i:
-            # if yes then add no of smaller problems to result.
-            count += i
-            # create smaller problem
-            n = n / i
-    return count
+    if n == 1:
+            return 0
+    if n == 2:
+        return 2
+    dp = [float('inf')] * (n+1)  # dp[i] denote minimal operations for i 'A'
+    # print(dp, '1')
+    dp[1] = 0
+    dp[2] = 2
+    # print(dp, '2')
+    for i in range(3, n+1):
+        for j in range(1, (i//2)+1):
+            # copy from anywhere when the number of missing 'A' (i-j)
+            # is dividable by the number of existing 'A' (j)
+            # the bottom line is when j == 1
+            # 1 copy + (i-j)/j paste
+            # print(i, j)
+            if (i - j) % j == 0:
+                dp[i] = min(dp[i], dp[j] + 1 + (i-j)//j)
+                # print(dp, '3')
+    return dp[n]
+    # count = 0
+    # for i in range(2, n + 1):
+    #     # check if problem can be broken into smaller problem
+    #     while not n % i:
+    #         # if yes then add no of smaller problems to result.
+    #         count += i
+    #         # create smaller problem
+    #         n /= i
+    # return count
--- 0x02-minimum_operations/main.py
+++ 0x02-minimum_operations/main.py
@@ -5,8 +5,8 @@ Main file for testing
 
 minOperations = __import__('0-minoperations').minOperations
 
-n = 9
+n = 4
 print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
 
-n = 50
+n = 9
 print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
