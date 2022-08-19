07fff7a4f23b299dd97378b0ea3c0216fca7a2b0
Joseun <ologanj@gmail.com>
2022-08-19 00:50:51 +0000

    Island Perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -15,16 +15,14 @@ def island_perimeter(grid):
     n_cols = len(grid[0])
     row = 1
     sides_count = []
-    while row <= n_rows - 2:
-        for col in range(1, n_cols - 2):
+    for row in range(n_rows):
+        for col in range(n_cols):
             if grid[row][col] == 1:
                 up = grid[row - 1][col]
                 down = grid[row + 1][col]
                 left = grid[row][col - 1]
                 right = grid[row][col + 1]
                 total = up + down + left + right
-                if total == 0:
-                    multiple_islands.append(1)
                 sides_count.append(4 - total)
         row += 1
     perimeter = sum(sides_count)
